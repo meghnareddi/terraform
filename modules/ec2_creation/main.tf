7306f0339a2068f05c8a914c4a6696c5f73787e4
unknown <megh221322@gmail.com>
2025-08-04 16:29:24 -0400

    ec2 root .tf modified

--- modules/ec2_creation/main.tf
+++ modules/ec2_creation/main.tf
@@ -1,9 +1,14 @@
+resource "time_sleep" "delay_before_destroy" {
+  destroy_duration = "60s" 
+}
+
+
 resource "aws_instance" "mr_ec2" {
   count         = var.instance_count
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
-
+  depends_on = [time_sleep.delay_before_destroy]
   tags = {
     Name        = "${var.ec2_name}-${count.index + 1}"
     Environment = var.environment
